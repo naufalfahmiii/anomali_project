6a38ffaad02f4114d3bd57c2c58a3a5aecb8950b
naufalfahmiii <143859276+naufalfahmiii@users.noreply.github.com>
2023-09-02 10:42:39 +0700

    my first commit

---
+++ helloworld.py
@@ -0,0 +1,31 @@
+# apel = 60
+# print(apel)
+# print(601)
+# nama_apel = "Apel Malang"
+# nama_apel = 
+# a1pel =
+# apel_benar = True
+# apel_salah = False
+# print(type(apel_benar))
+# print(apel_salah)
+# list_warna_apel = ['hijau','merah','kuning']
+# list_apel1 = ['manis', 2 , 3.0]
+# print(list_warna_apel)
+# print(list_apel1)
+# tuple_jumlah = (2, 5.25, "satu")
+# print(tuple_jumlah)
+apel_dictionary = {'nama' : 'coklat','asal' : 'sidoarjo','harga' : '100'}
+print(apel_dictionary['nama'])
+print(apel_dictionary['asal'])
+print(apel_dictionary['harga'])
+# print(34+5)
+# print(39-5)
+# print(2*8)
+# print(10/2)
+# b = 80
+# c = 10
+# print(b/c)
+# x = 50
+# z = 8
+# print(x//z)
+

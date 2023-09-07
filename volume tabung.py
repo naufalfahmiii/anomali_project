572b0e19c09a6c3d75507e2b4e9f68355a6a0a14
naufalfahmiii <143859276+naufalfahmiii@users.noreply.github.com>
2023-09-07 19:21:04 +0700

    Add files via upload
    pelatihan coding 1

---
+++ volume tabung.py
@@ -0,0 +1,9 @@
+print("program menghitung volume tabung")
+
+# menerima input jari-jari dan tinggi tabung
+jari_jari = int(input("masukkan jari-jari tabung :"))
+tinggi = int(input("masukkan tinggi tabung :"))
+phi = 22/7
+
+# menghitung volume tabung
+volume_tabung = phi * jari_jari * 2 * tinggi
